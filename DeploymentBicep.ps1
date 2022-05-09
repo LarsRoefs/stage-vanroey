9d87a2af1d26a7e561062ea2694b69673b929557
LarsRoefs <61016324+LarsRoefs@users.noreply.github.com>
2022-05-09 10:53:17 +0200

    Delete DeploymentBicep.ps1

--- DeploymentBicep.ps1
+++
@@ -1,9 +0,0 @@
-$resourceGroup = "Bicep"
-$path = "master.bicep"
-
-New-AzResourceGroup -Name $ResourceGroup -Location "westeurope"
-
-New-AzResourceGroupDeployment `
-  -Name 'new-bicep-deploy' `
-  -ResourceGroupName $ResourceGroup `
-  -TemplateFile $path
